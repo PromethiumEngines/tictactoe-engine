8d8c39585211c0b025652a23535fe7948c335c85
VyrekXD <vyrekxd@gmail.com>
2023-03-05 13:15:07 -0600

    Fix: gofmt ✏

--- tictactoe_test.go
+++ tictactoe_test.go
@@ -8,18 +8,18 @@ import (
 
 // Test3x3<>Match calls tictactoe.New() with the default options of ToWin 3, Height 3, Weight 3
 // for a valid return value, 0 should win in horizontal, vertical and 1 should win in diagonal
-func Test3x3HorizontalMatch(t *testing.T){
+func Test3x3HorizontalMatch(t *testing.T) {
 	start := time.Now()
 	tttGame, err := New(&GameOptions{ToWin: 3, Height: 3, Weight: 3})
 	if err != nil {
 		t.Fatalf("New(), options are invalid, horizontal match, %v, want valid GameOptions", err)
 	}
 
-	tttGame.OnWinner(func (g *Game) {
+	tttGame.OnWinner(func(g *Game) {
 		log.Printf(`Horizontal game concluded, "%v" wins, "%v" turns. (In %v)`, *g.Winner, g.Turn, time.Since(start))
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		t.Fatalf(`Horizontal game concluded as a draw, in "%v" turns.`, g.Turn)
 	})
 
@@ -44,11 +44,11 @@ func Test3x3VerticalMatch(t *testing.T) {
 		t.Fatalf("New(), options are invalid, vertical match, %v, want valid GameOptions", err)
 	}
 
-	tttGame.OnWinner(func (g *Game) {
+	tttGame.OnWinner(func(g *Game) {
 		log.Printf(`Vertical game concluded, "%v" wins, "%v" turns. (In %v)`, *g.Winner, g.Turn, time.Since(start))
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		t.Fatalf(`Vertical game concluded as a draw, in "%v" turns.`, g.Turn)
 	})
 
@@ -66,18 +66,18 @@ func Test3x3VerticalMatch(t *testing.T) {
 	tttGame.AddMove(0, 6)
 }
 
-func Test3x3DiagonalMatch(t *testing.T){
+func Test3x3DiagonalMatch(t *testing.T) {
 	start := time.Now()
 	tttGame, err := New(&GameOptions{ToWin: 3, Height: 3, Weight: 3})
 	if err != nil {
 		t.Fatalf("New(), options are invalid, diagonal match, %v, want valid GameOptions", err)
 	}
 
-	tttGame.OnWinner(func (g *Game) {
+	tttGame.OnWinner(func(g *Game) {
 		log.Printf(`Diagonal game concluded, "%v" wins, "%v" turns. (In %v)`, *g.Winner, g.Turn, time.Since(start))
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		t.Fatalf(`Diagonal game concluded as a draw, in "%v" turns.`, g.Turn)
 	})
 
@@ -95,7 +95,7 @@ func Test3x3DiagonalMatch(t *testing.T){
 	tttGame.AddMove(1, 8)
 }
 
-func Test3x3DrawMatch(t *testing.T){
+func Test3x3DrawMatch(t *testing.T) {
 	start := time.Now()
 	tttGame, err := New(&GameOptions{ToWin: 3, Height: 3, Weight: 3})
 	if err != nil {
@@ -106,7 +106,7 @@ func Test3x3DrawMatch(t *testing.T){
 		t.Fatalf(`Draw game concluded as a win, "%v" wins, in "%v" turns.`, *g.Winner, g.Turn)
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		log.Printf(`Draw game concluded correctly, in "%v" turns. (In %v)`, g.Turn, time.Since(start))
 	})
 
@@ -131,18 +131,18 @@ func Test3x3DrawMatch(t *testing.T){
 	tttGame.AddMove(0, 8)
 }
 
-func Test5x4HorizontalMatch(t *testing.T){
+func Test5x4HorizontalMatch(t *testing.T) {
 	start := time.Now()
 	tttGame, err := New(&GameOptions{ToWin: 4, Height: 4, Weight: 5})
 	if err != nil {
 		t.Fatalf("New(), options are invalid, horizontal 5x4 match, %v, want valid GameOptions", err)
 	}
 
-	tttGame.OnWinner(func (g *Game) {
+	tttGame.OnWinner(func(g *Game) {
 		log.Printf(`Horizontal 5x4 game concluded, "%v" wins, "%v" turns. (In %v)`, *g.Winner, g.Turn, time.Since(start))
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		t.Fatalf(`Horizontal 5x4 game concluded as a draw, in "%v" turns.`, g.Turn)
 	})
 
@@ -171,11 +171,11 @@ func Test5x4VerticalMatch(t *testing.T) {
 		t.Fatalf("New(), options are invalid, horizontal 5x4 match, %v, want valid GameOptions", err)
 	}
 
-	tttGame.OnWinner(func (g *Game) {
+	tttGame.OnWinner(func(g *Game) {
 		log.Printf(`Vertical 5x4 game concluded, "%v" wins, "%v" turns. (In %v)`, *g.Winner, g.Turn, time.Since(start))
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		t.Fatalf(`Vertical 5x4 game concluded as a draw, in "%v" turns.`, g.Turn)
 	})
 
@@ -190,9 +190,9 @@ func Test5x4VerticalMatch(t *testing.T) {
 	tttGame.AddMove(1, 2)
 
 	tttGame.AddMove(0, 10)
-	
+
 	tttGame.AddMove(1, 3)
-	
+
 	// Here X should win
 	tttGame.AddMove(0, 15)
 }
@@ -204,11 +204,11 @@ func Test5x4DiagonalMatch(t *testing.T) {
 		t.Fatalf("New(), options are invalid, horizontal 5x4 match, %v, want valid GameOptions", err)
 	}
 
-	tttGame.OnWinner(func (g *Game) {
+	tttGame.OnWinner(func(g *Game) {
 		log.Printf(`Diagonal 5x4 game concluded, "%v" wins, "%v" turns. (In %v)`, *g.Winner, g.Turn, time.Since(start))
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		t.Fatalf(`Diagonal 5x4 game concluded as a draw, in "%v" turns.`, g.Turn)
 	})
 
@@ -223,9 +223,9 @@ func Test5x4DiagonalMatch(t *testing.T) {
 	tttGame.AddMove(0, 2)
 
 	tttGame.AddMove(1, 12)
-	
+
 	tttGame.AddMove(0, 3)
-	
+
 	// Here O should win
 	tttGame.AddMove(1, 18)
 }
@@ -241,7 +241,7 @@ func Test5x4DrawMatch(t *testing.T) {
 		t.Fatalf(`Draw 5x4 game concluded as a win, "%v" wins, in "%v" turns.`, *g.Winner, g.Turn)
 	})
 
-	tttGame.OnDraw(func (g *Game) {
+	tttGame.OnDraw(func(g *Game) {
 		log.Printf(`Draw 5x4 game concluded correctly, in "%v" turns. (In %v)`, g.Turn, time.Since(start))
 	})
 
@@ -288,5 +288,5 @@ func Test5x4DrawMatch(t *testing.T) {
 	tttGame.AddMove(1, 18)
 
 	// The match will end as draw here
-	tttGame.AddMove(0, 19)	
+	tttGame.AddMove(0, 19)
 }
